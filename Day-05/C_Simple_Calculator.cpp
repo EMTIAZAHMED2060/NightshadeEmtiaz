ec34cb8e0b540589537aafbbc8aa322c67ca9a37
EMTIAZAHMED2060 <emtiaz.ahmed@g.bracu.ac.bd>
2024-01-06 12:38:18 +0600

    day-06

---
+++ Day-05/C_Simple_Calculator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, j;
+    cin >> i >> j;
+
+    cout << i << " + " << j << " = " << i + j << endl;
+    cout << i << " * " << j << " = " << i * j << endl;
+    cout << i << " - " << j << " = " << i - j << endl;
+
+    return 0;
+}
